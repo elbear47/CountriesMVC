2da715e6770ea98fd0764c82e0041dbfd383852c
Elber Funez <elberfunez@gmail.com>
2022-08-25 22:13:05 -0400

    finished lab

---
+++ CountriesMVC/CountryView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CountriesMVC
+{
+	public class CountryView
+	{
+        public Country DisplayCountry { get; set; }
+        public CountryView(Country country)
+		{
+			DisplayCountry = country;
+		}
+
+		public void Display()
+        {
+			string colors = "";
+			DisplayCountry.Colors.ForEach(x => colors += x + ", ");
+
+            Console.WriteLine("Country Name: " + DisplayCountry.Name);
+			Console.WriteLine("Continent: " + DisplayCountry.Continent);
+			Console.WriteLine("Colors: " + colors);
+		}
+	}
+}
+
--- CountriesMVC/Program.cs
+++ CountriesMVC/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
+using CountriesMVC;
+
+CountryController cc = new CountryController();
+
+cc.WelcomeAction();
